0d0f517efa12d1e8a4397772d43efb5fa5340d90
arie <prasetyo.mailbox@gmail.com>
2019-03-11 15:14:07 +0700

    initial project

---
+++ Swift Learning/BaseViewController.swift
@@ -0,0 +1,48 @@
+//
+//  BaseViewController.swift
+//  Swift Learning
+//
+//  Created by Vanbungkring on 3/11/19.
+//  Copyright © 2019 netra. All rights reserved.
+//
+
+import UIKit
+extension UIViewController {
+    func showAlert(title:String,msg:String){
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            switch action.style{
+            case .default:
+                print("default")
+                
+            case .cancel:
+                print("cancel")
+                
+            case .destructive:
+                print("destructive")
+                
+                
+            }}))
+        self.present(alert, animated: true, completion: nil)
+    };
+}
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
