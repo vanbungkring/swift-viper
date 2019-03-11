29e832c4327d2ddb37c394a209d655ce3529e294
arie <prasetyo.mailbox@gmail.com>
2019-03-11 11:53:42 +0700

    Initial Commit

---
+++ Swift Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Learning
+//
+//  Created by Vanbungkring on 3/11/19.
+//  Copyright © 2019 netra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
